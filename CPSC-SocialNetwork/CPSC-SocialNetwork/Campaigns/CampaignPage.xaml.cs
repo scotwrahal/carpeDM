402d9f139d0725021e71ca02c62f5e68de896bdd
Kristopher Lugtu <kristopherl.lugtu@gmail.com>
2018-11-26 15:41:23 -0700

    Fixed Tab Sizes. Change Font Family and Font Size.

--- CPSC-SocialNetwork/CPSC-SocialNetwork/Campaigns/CampaignPage.xaml.cs
+++ CPSC-SocialNetwork/CPSC-SocialNetwork/Campaigns/CampaignPage.xaml.cs
@@ -24,5 +24,10 @@ namespace CPSC_SocialNetwork.Campaigns
         {
             InitializeComponent();
         }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
